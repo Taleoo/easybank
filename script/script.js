9ba0105d0c5ce8d0455801530cdd49062541d407
Taleoo <mehdi.goudjil@gmail.com>
2020-10-30 12:24:17 +0100

    Final touches

--- script/script.js
+++ script/script.js
@@ -3,8 +3,8 @@ $( document ).ready(function() {
         $("#menu").click(function () {
         if ($('#mobilemenu').length < 1){
             $("#phoneimage").append($("<div />", { id: "mobilemenu" }));
-            $("#mobilemenu").append($("#menulist"));
-            $("#menulist").css("display", "inline-block");
+            $( ".menulist" ).clone().appendTo( "#mobilemenu" );
+            $("#mobilemenu.menulist").css("display", "inline-block");
             } else {$('#mobilemenu').toggle();
             };
         });
